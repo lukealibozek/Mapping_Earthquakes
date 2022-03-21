e150452ef109cefaff623a7756cb4c724a37c71a
lukealibozek-cAXrSb <lukealibozek@gmail.com>
2022-03-20 20:42:43 -0500

    match to module

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,86 +1,22 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-<<<<<<< HEAD
-// Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
-=======
-// let map = L.map('mapid').setView([40.7, -94.5], 4);
->>>>>>> 5280382bc7d5cf1bc49f9444569b3741d1a9fa10
-// let map = L.map('mapid').setView([34.0522, -118.2437], 15);
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+// Create the map objec
+let map = L.map('mapid').setView([30,30],2);
 
-// -----------alterantive--------------
-// Create the map object with a center and zoom level.
-// let map = L.map("mapid", {
-//     center: [
-//       40.7, -94.5
-//     ],
-//     zoom: 4
-//   });
-
-//  Add a marker to the map for Los Angeles, California.
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-// L.circle([34.0522, -118.2437], {
-//     color: 'black',
-//     fillColor: 'yellow',
-//     radius: 300
-//  }).addTo(map);
-
-// Get data from cities.js
-let cityData = cities;
-
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000,
-        fillColor: 'orange',
-        lineWeight: '4',
-        color: 'black'
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
-
-// Coordinates for each point to be used in the line.
-// Coordinates for each point to be used in the polyline.
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-  ];
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "yellow"
-  }).addTo(map);
-//  var circle = L.circle([51.508, -0.11], {
-//     color: 'red',
-//     fillColor: '#f03',
-//     fillOpacity: 0.5,
-//     radius: 500
-// }).addTo(map);
-
-// We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-// Accessing the airport GeoJSON URL
+streets.addTo(map);
+
 let airportData = "https://raw.githubusercontent.com/lukealibozek/Mapping_Earthquakes/main/majorAirports.json";
 
-// Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data).addTo(map);
 });
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
