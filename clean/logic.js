ddea1779d9fe71efd900c75e98fba2ebb4e75948
lukealibozek-cAXrSb <lukealibozek@gmail.com>
2022-03-22 23:31:07 -0500

    final upload

--- clean/logic.js
+++ clean/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map objec
-let map = L.map('mapid').setView([30,30],2);
+let map = L.map('mapid').setView([44.0, -80.0],5);
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -14,9 +14,13 @@ streets.addTo(map);
 
 let airportData = "https://raw.githubusercontent.com/lukealibozek/Mapping_Earthquakes/main/majorAirports.json";
 
-d3.json(airportData).then(function(data) {
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data).addTo(map);
 });
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/torontoRoutes.json";
 
